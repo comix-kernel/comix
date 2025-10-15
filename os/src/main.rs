de7fa2f0a85499222ee691c400b3356a77e8ab78
miiyakumo <miiyakumo@qq.com>
2025-10-15 08:39:06 +0000

    feat: 搭建基本裸机环境

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
+#![no_std]
+#![no_main]
+
+#[macro_use]
+mod console;
+mod sbi;
+
+use core::arch::global_asm;
+use core::panic::PanicInfo;
+use crate::sbi::shutdown;
+
+
+
+global_asm!(include_str!("entry.asm"));
+
+#[unsafe(no_mangle)]
+pub fn rust_main() -> ! {
+    clear_bss();
     println!("Hello, world!");
+    shutdown(false);
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message()
+        );
+    } else {
+        println!("Panicked: {}", info.message());
+    }
+    shutdown(true)
+}
+
+fn clear_bss() {
+    unsafe extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
 }
