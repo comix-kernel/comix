3b930f93035d6a3552446a36133216c0e29599ac
ZIYAN137 <zi_yan137@outlook.com>
2025-10-18 14:03:26 +0000

    feat(mm): 实现Vpn和Ppn

--- os/src/mm/address/mod.rs
+++ os/src/mm/address/mod.rs
@@ -1,5 +1,7 @@
 mod operations;
 mod address;
+mod page_num;
 
 pub use operations::{AlignOps, CalcOps, UsizeConvert};
-pub use address::{Vaddr, Paddr};
+pub use address::{Vaddr, Paddr, Address};
+pub use page_num::{Vpn, Ppn, PageNum};
---
+++ os/src/mm/address/page_num.rs
@@ -0,0 +1,80 @@
+use crate::config::PAGE_SIZE;
+use crate::mm::address::address::{Address, Vaddr, Paddr};
+use crate::mm::address::operations::{AlignOps, CalcOps, UsizeConvert};
+
+// TODO: implement the trait
+// trait to represent an page number
+pub trait PageNum:
+    CalcOps + UsizeConvert + Copy + Clone + PartialEq + PartialOrd + Eq + Ord
+{
+    type TAddress: Address;
+
+    fn step(&mut self) {
+        self.step_by(1);
+    }
+
+    fn step_by(&mut self, offset: usize) {
+        *self = Self::from_usize(self.as_usize() + offset);
+    }
+
+    fn step_back(&mut self) {
+        self.step_back_by(1);
+    }
+
+    fn step_back_by(&mut self, offset: usize) {
+        *self = Self::from_usize(self.as_usize() - offset);
+    }
+
+    fn from_addr_floor(addr: Self::TAddress) -> Self {
+        Self::from_usize(addr.align_down_to_page().as_usize() / PAGE_SIZE)
+    }
+
+    fn from_addr_ceil(addr: Self::TAddress) -> Self {
+        Self::from_usize(addr.align_up_to_page().as_usize() / PAGE_SIZE)
+    }
+
+    fn start_addr(self) -> Self::TAddress {
+        Self::TAddress::from_usize(self.as_usize() * PAGE_SIZE)
+    }
+
+    fn end_addr(self) -> Self::TAddress {
+        Self::TAddress::from_usize((self.as_usize() + 1) * PAGE_SIZE)
+    }
+
+    fn diff(self, other: Self) -> isize {
+        self.as_usize() as isize - other.as_usize() as isize
+    }
+}
+
+#[macro_export]
+macro_rules! impl_page_num {
+    ($type:ty, $addr_type:ty) => {
+        impl $crate::mm::address::operations::UsizeConvert for $type {
+            fn as_usize(&self) -> usize {
+                self.0
+            }
+
+            fn from_usize(value: usize) -> Self {
+                Self(value)
+            }
+        }
+
+        $crate::impl_calc_ops!($type);
+
+        impl $crate::mm::address::page_num::PageNum for $type {
+            type TAddress = $addr_type;
+        }
+    };
+}
+
+// phyical page number
+#[repr(transparent)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Ppn(pub usize);
+impl_page_num!(Ppn, Paddr);
+
+// virtual page number
+#[repr(transparent)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Vpn(pub usize);
+impl_page_num!(Vpn, Vaddr);
