7ea95077e31f62d1d03433d80ea6df20434d5d9c
miiyakumo <miiyakumo@qq.com>
2025-10-20 13:39:38 +0000

    feat: 添加处理内核中断的初步代码

---
+++ os/src/arch/riscv/trap/kerneltrap.rs
@@ -0,0 +1,68 @@
+// 64 位 RISC-V，usize = 8 字节
+
+use riscv::register::{sepc, sstatus};
+
+#[unsafe(no_mangle)]
+pub extern "C" fn kerneltrap(_trap_frame: &mut KernelTrapFrame) {
+    // 陷阱帧的地址（sp）被隐式地作为参数 a0 传递给了 kerneltrap
+    // 在这里，trap_frame 指向了栈上保存的 KernelTrapFrame 结构体
+    use riscv::register::scause;
+
+    // XXX: 有被中断的风险？或许应将这一段作为参数传递进来
+    let scause = scause::read();
+    let sepc = sepc::read();
+    let sstatus = sstatus::read();
+
+    match scause.cause() {
+        // 不是我喜欢的异常，直接panic
+        _ => panic!("Unexpected trap in kernel: scause = {:?}, sepc = {:#x}, sstatus = {:#x}",
+                    scause.cause(),
+                    sepc,
+                    sstatus.bits()),
+    }
+
+    // XXX: CSR可能因调度被修改？
+}
+
+#[repr(C)] // 确保 Rust 不会重新排列字段
+#[derive(Debug, Clone, Copy)]
+pub struct KernelTrapFrame {
+    pub x1_ra: usize,  // 0(sp)
+    pub x2_sp: usize,  // 8(sp)
+    pub x3_gp: usize,  // 16(sp)
+    pub x4_tp: usize,  // 24(sp)
+    pub x5_t0: usize,  // 32(sp)
+    pub x6_t1: usize,  // 40(sp)
+    pub x7_t2: usize,  // 48(sp)
+    pub x8_s0: usize,  // 56(sp)
+    pub x9_s1: usize,  // 64(sp)
+    pub x10_a0: usize, // 72(sp)
+    pub x11_a1: usize, // 80(sp)
+    pub x12_a2: usize, // 88(sp)
+    pub x13_a3: usize, // 96(sp)
+    pub x14_a4: usize, // 104(sp)
+    pub x15_a5: usize, // 112(sp)
+    pub x16_a6: usize, // 120(sp)
+    pub x17_a7: usize, // 128(sp)
+    pub x18_s2: usize, // 136(sp)
+    pub x19_s3: usize, // 144(sp)
+    pub x20_s4: usize, // 152(sp)
+    pub x21_s5: usize, // 160(sp)
+    pub x22_s6: usize, // 168(sp)
+    pub x23_s7: usize, // 176(sp)
+    pub x24_s8: usize, // 184(sp)
+    pub x25_s9: usize, // 192(sp)
+    pub x26_s10: usize, // 200(sp)
+    pub x27_s11: usize, // 208(sp)
+    pub x28_t3: usize, // 216(sp)
+    pub x29_t4: usize, // 224(sp)
+    pub x30_t5: usize, // 232(sp)
+    pub x31_t6: usize, // 240(sp)
+    // 总共 31 个寄存器，总大小 31 * 8 = 248 字节。
+    // 汇编代码分配了 256 字节，最后 8 字节未使用。
+}
+
+/// TODO: 处理时钟中断
+pub fn check_timer() {}
+/// TODO: 处理设备中断
+pub fn check_device() {}
---
+++ os/src/arch/riscv/trap/mod.rs
@@ -0,0 +1,28 @@
+mod kerneltrap;
+
+use core::arch::global_asm;
+use riscv::register::{
+    mtvec::TrapMode, sie, stvec::{self, Stvec},
+};
+
+global_asm!(include_str!("kernelvec.S"));
+
+unsafe extern "C" {
+    unsafe fn kernelvec(); 
+}
+
+pub fn init() {
+    set_kernel_trap_entry();
+}
+
+fn set_kernel_trap_entry() {
+    unsafe {
+        stvec::write(Stvec::new(kernelvec as usize, TrapMode::Direct));
+    }
+}
+
+pub fn enable_timer_interrupt() {
+    unsafe {
+        sie::set_stimer();
+    }
+}
