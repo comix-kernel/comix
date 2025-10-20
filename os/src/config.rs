42bb53975ce85a3140284d68fd4b1e7e6ea22972
miiYakumo <142552784+miiyakumo@users.noreply.github.com>
2025-10-20 21:34:43 +0800

    fix: Update import path for QEMU platform
    修改导致编译错误的引入路径

--- os/src/config.rs
+++ os/src/config.rs
@@ -6,4 +6,4 @@ pub const PAGE_SIZE: usize = 4096;
 pub const KERNEL_HEAP_SIZE: usize = 16 * 1024 * 1024; // 16MB
 pub const USER_STACK_SIZE: usize = 4 * 1024 * 1024; // 4MB
 
-pub use crate::arch::riscv::platform::qemu::*;
+pub use crate::arch::platform::qemu::*;
