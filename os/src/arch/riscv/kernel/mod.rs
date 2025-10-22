8482720f36aa389ecaa18f759ec1ec3e101d1cfe
miiyakumo <miiyakumo@qq.com>
2025-10-22 12:26:08 +0000

    feat: 初步实现调度器接口与轮询调度器

--- os/src/arch/riscv/kernel/mod.rs
+++ os/src/arch/riscv/kernel/mod.rs
@@ -1,13 +1,15 @@
-pub mod context;
-
 use core::arch::global_asm;
+
+use crate::arch::kernel::context::Context;
+
+pub mod context;
 
 global_asm!(include_str!("switch.S"));
 
 unsafe extern "C" {
     /// 上下文切换函数
-    /// 
+    ///
     /// 保存当前任务的寄存器状态到 old 指向的 context 结构体中，
     /// 然后从 new 指向的 context 结构体中恢复寄存器状态，切换到新任务执行。
-    pub fn switch(old: *mut context::Context, new: *const context::Context);
+    pub fn switch(old: *mut Context, new: *const Context);
 }
