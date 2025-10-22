f4a738bb2d892e3fc736824c16249f38e9320fb0
miiyakumo <miiyakumo@qq.com>
2025-10-22 09:48:18 +0000

    feat: 实现进程上下文切换相关代码

--- os/src/arch/riscv/kernel/mod.rs
+++ os/src/arch/riscv/kernel/mod.rs
@@ -1,1 +1,13 @@
 pub mod context;
+
+use core::arch::global_asm;
+
+global_asm!(include_str!("switch.S"));
+
+unsafe extern "C" {
+    /// 上下文切换函数
+    /// 
+    /// 保存当前任务的寄存器状态到 old 指向的 context 结构体中，
+    /// 然后从 new 指向的 context 结构体中恢复寄存器状态，切换到新任务执行。
+    pub fn switch(old: *mut context::Context, new: *const context::Context);
+}
