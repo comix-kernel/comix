6cc0fe1e53803316e49b8f869ab4448be3ee84b8
ZIYAN137 <zi_yan137@outlook.com>
2025-10-22 22:52:37 +0000

    feat: 创建MemorySpace的结构

---
+++ os/src/mm/memory_space/memory_space.rs
@@ -0,0 +1,11 @@
+use alloc::vec::Vec;
+use crate::mm::page_table::ActivePageTableInner;
+use crate::mm::memory_space::mapping_area::MappingArea;
+
+static mut KERNEL_SPACE: Option<MemorySpace> = None;
+
+pub struct MemorySpace {
+    pub page_table: ActivePageTableInner,
+    pub mapping_areas: Vec<MappingArea>,
+}
+
