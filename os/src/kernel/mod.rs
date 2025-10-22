52ad7799edbe6eb60766bb40b941823891abfa03
miiyakumo <miiyakumo@qq.com>
2025-10-22 08:34:27 +0000

    feature: 实现简单的任务id分配器

--- os/src/kernel/mod.rs
+++ os/src/kernel/mod.rs
@@ -1,2 +1,4 @@
 pub mod task_info;
 pub mod task_struct;
+
+mod tid_allocator;
---
+++ os/src/kernel/tid_allocator.rs
@@ -0,0 +1,24 @@
+#![allow(dead_code)]
+use core::sync::atomic::AtomicU32;
+
+/// 简单的任务ID分配器。
+/// 每次调用`allocate`方法时，都会返回一个唯一的任务ID。
+/// 任务ID从1开始递增。
+#[derive(Debug)]
+pub struct TidAllocator {
+    next_tid: AtomicU32,
+}
+
+impl TidAllocator {
+    /// 创建一个新的TidAllocator实例。
+    pub const fn new() -> Self {
+        TidAllocator {
+            next_tid: AtomicU32::new(1),
+        }
+    }
+
+    /// 分配一个新的任务ID。
+    pub fn allocate(&self) -> u32 {
+        self.next_tid.fetch_add(1, core::sync::atomic::Ordering::SeqCst)
+    }
+}
