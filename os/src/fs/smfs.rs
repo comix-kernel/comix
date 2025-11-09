ec5d73b5826ffc594ced4551e54bc75bee053ddf
miiyakumo <miiyakumo@qq.com>
2025-11-09 14:12:31 +0000

    docs: 更新 README.md，添加用户程序静态加载说明

--- os/src/fs/smfs.rs
+++ os/src/fs/smfs.rs
@@ -26,23 +26,23 @@ pub struct SimpleMemoryFileSystem {
 struct Align8<const N: usize>([u8; N]);
 
 // 用 include_bytes! 宏将编译好的用户程序嵌入到这里
-const INIT: Align8<{ include_bytes!("../../../user/bin/init").len() }> =
-    Align8(*include_bytes!("../../../user/bin/init"));
-static HELLO: Align8<{ include_bytes!("../../../user/bin/hello").len() }> =
-    Align8(*include_bytes!("../../../user/bin/hello"));
+// const INIT: Align8<{ include_bytes!("../../../user/bin/init").len() }> =
+//     Align8(*include_bytes!("../../../user/bin/init"));
+// static HELLO: Align8<{ include_bytes!("../../../user/bin/hello").len() }> =
+//     Align8(*include_bytes!("../../../user/bin/hello"));
 
 /// 静态文件列表：这是 MemFS 的核心存储
-static STATIC_FILES: [FileEntry; 2] = [
-    FileEntry {
-        name: "init",
-        data: &INIT.0,
-        size: INIT.0.len(),
-    },
-    FileEntry {
-        name: "hello",
-        data: &HELLO.0,
-        size: HELLO.0.len(),
-    },
+static STATIC_FILES: [FileEntry; 0] = [
+    // FileEntry {
+    //     name: "init",
+    //     data: &INIT.0,
+    //     size: INIT.0.len(),
+    // },
+    // FileEntry {
+    //     name: "hello",
+    //     data: &HELLO.0,
+    //     size: HELLO.0.len(),
+    // },
 ];
 
 impl SimpleMemoryFileSystem {
