32754ee81be649470715dacfc8c577f9dcbb207c
ZIYAN137 <zi_yan137@outlook.com>
2025-11-12 05:35:24 +0000

    feat: 实现 File对象 和 FDTable

--- os/src/vfs/mod.rs
+++ os/src/vfs/mod.rs
@@ -6,4 +6,6 @@ pub mod inode;
 
 pub use dentry::{DENTRY_CACHE, Dentry, DentryCache};
 pub use error::FsError;
+pub use fd_table::FDTable;
+pub use file::{File, OpenFlags, SeekWhence};
 pub use inode::{DirEntry, FileMode, Inode, InodeMetadata, InodeType, TimeSpec};
