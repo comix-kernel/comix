afa075a98f8113dc67503b1ac0deb779885eac3d
LittleSand <1840309785@qq.com>
2025-11-18 21:07:46 +0800

    fix:cargo fmt

--- os/src/devices/mod.rs
+++ os/src/devices/mod.rs
@@ -1,13 +1,13 @@
 //! 设备抽象层，提供块设备接口、内存磁盘实现和网络设备接口
 
 pub mod block_device;
-pub mod ram_disk;
 pub mod net_device;
+pub mod ram_disk;
 pub mod virtio_hal;
 
 pub use block_device::{BlockDevice, BlockError};
+pub use net_device::NetDevice;
 pub use ram_disk::RamDisk;
-pub use net_device::{NetDevice};
 pub use virtio_hal::VirtIOHal;
 
 #[cfg(test)]
--- os/src/devices/net_device.rs
+++ os/src/devices/net_device.rs
@@ -12,27 +12,30 @@ pub enum NetDeviceError {
 pub trait NetDevice: Send + Sync {
     /// 发送数据包
     fn send(&self, packet: &[u8]) -> Result<(), NetDeviceError>;
-    
+
     /// 接收数据包
     fn receive(&self, buf: &mut [u8]) -> Result<usize, NetDeviceError>;
-    
+
     /// 获取设备标识符
     fn device_id(&self) -> usize;
-    
+
     /// 获取最大传输单元(MTU)
     fn mtu(&self) -> usize;
-    
+
     /// 获取设备名称
     fn name(&self) -> &str;
-    
+
     /// 获取MAC地址
     fn mac_address(&self) -> [u8; 6];
 }
 
 use crate::devices::virtio_hal::VirtIOHal;
-use alloc::boxed::Box;
 use crate::sync::SpinLock;
-use virtio_drivers::{device::net::{TxBuffer, VirtIONet}, transport::Transport};
+use alloc::boxed::Box;
+use virtio_drivers::{
+    device::net::{TxBuffer, VirtIONet},
+    transport::Transport,
+};
 
 /// 使用 virtio-drivers 0.12.0 实现的 Virtio 网络设备
 pub struct VirtioNetDevice<T: Transport + Send + Sync> {
@@ -46,18 +49,18 @@ pub struct VirtioNetDevice<T: Transport + Send + Sync> {
 
 impl<T: Transport + Send + Sync> VirtioNetDevice<T> {
     /// 创建新的 Virtio 网络设备实例
-    /// 
+    ///
     /// # 参数
     /// * `transport` - VirtIO 设备传输层
     /// * `device_id` - 设备标识符
     pub fn new(transport: T, device_id: usize) -> Result<Box<Self>, NetDeviceError> {
         let virtio_net = VirtIONet::<VirtIOHal, T, 256>::new(transport, 0)
             .map_err(|_| NetDeviceError::DeviceNotReady)?;
-        
+
         // 从 VirtIONet 中获取 MAC 地址和 MTU
         let mac = virtio_net.mac_address();
         let mtu = 1500; // 默认以太网 MTU
-        
+
         Ok(Box::new(Self {
             virtio_net: SpinLock::new(Some(Box::new(virtio_net))),
             device_id,
@@ -71,16 +74,17 @@ impl<T: Transport + Send + Sync> VirtioNetDevice<T> {
 impl<T: Transport + Send + Sync> NetDevice for VirtioNetDevice<T> {
     /// 发送数据包
     fn send(&self, packet: &[u8]) -> Result<(), NetDeviceError> {
-        if packet.len() > self.mtu + 18 { // 18 字节用于以太网头部和尾部
+        if packet.len() > self.mtu + 18 {
+            // 18 字节用于以太网头部和尾部
             return Err(NetDeviceError::QueueFull);
         }
-        
+
         // 使用SpinLock的RAII模式，自动处理锁的获取和释放
         if let Some(ref mut virtio_net) = *self.virtio_net.lock() {
             let tx_buffer = TxBuffer::from(packet);
             match virtio_net.send(tx_buffer) {
                 Ok(_) => Ok(()),
-                Err(_) => Err(NetDeviceError::QueueFull)
+                Err(_) => Err(NetDeviceError::QueueFull),
             }
         } else {
             Err(NetDeviceError::DeviceNotReady)
@@ -98,7 +102,7 @@ impl<T: Transport + Send + Sync> NetDevice for VirtioNetDevice<T> {
                     let actual_len = core::cmp::min(packet.len(), buf.len());
                     buf[..actual_len].copy_from_slice(&packet[..actual_len]);
                     Ok(actual_len)
-                },
+                }
                 Err(_) => Err(NetDeviceError::QueueEmpty),
             }
         } else {
@@ -120,10 +124,9 @@ impl<T: Transport + Send + Sync> NetDevice for VirtioNetDevice<T> {
     fn name(&self) -> &str {
         self.name
     }
-    
+
     /// 获取MAC地址
     fn mac_address(&self) -> [u8; 6] {
         self.mac
     }
 }
-
