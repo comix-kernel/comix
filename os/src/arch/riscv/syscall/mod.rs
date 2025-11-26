ecf5b3a146787d373d3862eb6f3724f0f6d7400a
miiyakumo <miiyakumo@qq.com>
2025-11-26 15:05:42 +0000

    feat: 实现 sigprocmask 系统调用，支持信号屏蔽字的修改与查询

--- os/src/arch/riscv/syscall/mod.rs
+++ os/src/arch/riscv/syscall/mod.rs
@@ -41,6 +41,7 @@ pub fn dispatch_syscall(frame: &mut super::trap::TrapFrame) {
         syscall_number::SYS_PRLIMIT64 => sys_prlimit(frame),
         syscall_number::SYS_NANOSLEEP => sys_nanosleep(frame),
         syscall_number::SYS_RT_SIGPENDING => sys_sigpending(frame),
+        syscall_number::SYS_RT_SIGPROCMASK => sys_sigprocmask(frame),
         _ => {
             // 未知的系统调用
             frame.x10_a0 = (-2isize) as usize; // -ENOSYS
