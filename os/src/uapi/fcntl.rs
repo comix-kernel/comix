f4b556ca9484c5caef6255b19ab0a703985d4e2a
ZIYAN137 <zi_yan137@outlook.com>
2025-12-03 09:13:57 +0000

    style: 使用 cargo fmt 进行格式化

--- os/src/uapi/fcntl.rs
+++ os/src/uapi/fcntl.rs
@@ -271,7 +271,7 @@ bitflags! {
         /// exec 时关闭 (O_CLOEXEC)
         const O_CLOEXEC   = 0o2000000;
 
-        /// 大文件 (O_LARGEFILE) (空操作) 
+        /// 大文件 (O_LARGEFILE) (空操作)
         const O_LARGEFILE = 0o100000;
     }
 }
