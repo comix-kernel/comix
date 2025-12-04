c7d16441bd14c1d9dbb626c7504421c6e8cb3c75
ZIYAN137 <zi_yan137@outlook.com>
2025-12-04 07:32:33 +0000

    fix: 修复 auxv 对齐问题

--- os/src/kernel/task/task_struct.rs
+++ os/src/kernel/task/task_struct.rs
@@ -274,7 +274,7 @@ impl Task {
         //      否则必须实现类似 copy_to_user 的函数来完成拷贝,不然会引发页错误
         // 2. 设置用户栈布局，包含命令行参数和环境变量
         let (new_sp, argc, argv_vec_ptr, envp_vec_ptr) =
-            setup_stack_layout(sp_high, argv, envp, phdr_addr, phnum, phent);
+            setup_stack_layout(sp_high, argv, envp, phdr_addr, phnum, phent, entry_point);
 
         // 3. 配置 TrapFrame (新的上下文)
         // SAFETY: tfptr 指向的内存已经被分配且可写，并由 task 拥有
