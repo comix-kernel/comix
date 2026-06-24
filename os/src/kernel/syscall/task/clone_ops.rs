92bd179b3865ac91b6f0253f9321e1818927fedc
LittleSand <1840309785@qq.com>
2026-06-24 11:48:28 +0800

    syscall: fix riscv clone tls argument order

--- os/src/kernel/syscall/task/clone_ops.rs
+++ os/src/kernel/syscall/task/clone_ops.rs
@@ -12,8 +12,8 @@ pub fn clone(
     flags: c_ulong,   // a0: clone flags
     stack: c_ulong,   // a1: child stack pointer
     ptid: *mut c_int, // a2: parent_tid pointer
-    ctid: *mut c_int, // a3: child_tid pointer
-    tls: *mut c_void, // a4: TLS pointer
+    tls: *mut c_void, // a3: TLS pointer
+    ctid: *mut c_int, // a4: child_tid pointer
 ) -> c_int {
     let requested_flags = if let Some(requested_flags) = CloneFlags::from_bits(flags as usize) {
         requested_flags
